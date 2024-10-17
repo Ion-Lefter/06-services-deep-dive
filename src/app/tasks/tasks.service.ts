85f058fb426544e8664974adc9bb86911a73baec
Ion Lefter <ionlefter1997@gmail.com>
2024-10-17 10:25:27 +0300

    176. Using Angular's Dependency Injection Mechanism

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Task } from './task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  //constructor() { }
+
+  tasks = signal<Task[]>([]);
+
+  addTask(taskData:{title: string; description: string}){
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN'
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask])
+  }
 }
